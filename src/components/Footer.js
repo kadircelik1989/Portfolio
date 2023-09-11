adc9708c3fd3e838fba7d5260a8d15d124745af2
Kadir Celik <kadircelik_1989@icloud.com>
2023-09-11 18:48:07 +0200

    Footer Changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,7 +32,7 @@ export default function Footer() {
         <div className="copy">
           <LuCopyright></LuCopyright>
         </div>
-        <h3>Kadir Celik 1989</h3>
+        <h3>Kadir Celik 2003</h3>
       </div>
     </footer>
   );
